51bc71c82605153d4cee3d76d5d6930bb11342d0
Josh Archer <jarcher@greenriver.edu>
2023-04-20 16:43:01 -0700

    Adding initial files for lab.

---
+++ src/lab/LinkedListLab.java
@@ -0,0 +1,26 @@
+package lab;
+
+import nodes.Node;
+
+public class LinkedListLab
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public static Node createCircular(int[] elements)
+    {
+        return null;
+    }
+
+    public static Node[] split(Node head)
+    {
+        return null;
+    }
+
+    public static Node[] split(Node head, int length)
+    {
+        return null;
+    }
+}
